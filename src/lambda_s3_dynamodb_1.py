78e973f4f7dfa545f3d94c5bbe6c73e1f507b4f4
sumit-aws <sumitvu.2009@com>
2022-09-06 21:42:51 +0530

    added basic functionality

--- src/lambda_s3_dynamodb_1.py
+++ src/lambda_s3_dynamodb_1.py
@@ -2,4 +2,22 @@ import pandas as pd
 from datetime import date
 import boto3
 import os
+import configparser
 
+def initialize():
+    s3_client = boto3.client(service_name='s3')
+    return s3_client
+
+def upload_file():
+    filename= "data\sample_s3.yml"
+    initialize().upload_file(filename, read_config(), "load_sample_001.yml")
+
+def read_config():
+    config = configparser.RawConfigParser()
+    config.read('conf/config.properties')
+    raw_bucket = config.get('dev', 'raw_bucket')
+    print(raw_bucket)
+    return raw_bucket
+
+if __name__=="__main__":
+    upload_file()
